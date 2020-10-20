551676520ecd8a33d6272e4ae2027819899572cd
karthiksomashekhar <karthikhs1310@gmail.com>
2020-10-20 13:18:23 +0530

    Update App.java

--- demo/src/main/java/companyX/App.java
+++ demo/src/main/java/companyX/App.java
@@ -9,9 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.println( "Hello World Again" );
-	System.out.println( "Adding New Line Of Code" );
-	System.out.println( "Adding other Line of code to the project" );
-	System.out.println( "Adding other Line of code" );
     }
 }
