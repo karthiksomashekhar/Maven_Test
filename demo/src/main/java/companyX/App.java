e60697418e8321d19d3a09934357fbc2899d4e91
karthiksomashekhar <karthikhs1310@gmail.com>
2020-10-21 21:25:30 +0530

    This is from master

--- demo/src/main/java/companyX/App.java
+++ demo/src/main/java/companyX/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is from Master" );
+        System.out.println( "This is from Master code" );
     }
 }
