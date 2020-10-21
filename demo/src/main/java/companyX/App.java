22154ba6d6684cc681b4d8ce36b4f7640a0b0121
karthiksomashekhar <karthikhs1310@gmail.com>
2020-10-21 21:24:02 +0530

    Adding Test Branch code

--- demo/src/main/java/companyX/App.java
+++ demo/src/main/java/companyX/App.java
@@ -8,11 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-	System.out.println( "Hello World Again" );
-	System.out.println( "Adding New Line Of Code" );
-	System.out.println( "Adding other Line of code to the project" );
-	System.out.println( "Test Branch" );
-	    System.out.println( "Adding new line to Test Branch" );
+	  System.out.println( "This is Test Branch" );
     }
 }
