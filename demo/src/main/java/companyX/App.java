a58759f9393e375af1e7057df2cd42166837141e
karthiksomashekhar <karthikhs1310@gmail.com>
2020-10-28 16:15:10 +0530

    Adding or revereing back

--- demo/src/main/java/companyX/App.java
+++ demo/src/main/java/companyX/App.java
@@ -9,7 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Adding a First File" );
-	System.out.println( "Adding a New line" );
-	System.out.println( "Adding one more line to the code" );
     }
 }
