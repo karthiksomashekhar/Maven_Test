6fae1d31ebf5b46c879d79725714ef007f706a93
karthiksomashekhar <karthikhs1310@gmail.com>
2020-10-28 14:02:47 +0530

    Update App.java

--- demo/src/main/java/companyX/App.java
+++ demo/src/main/java/companyX/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
 	  System.out.println( "This is Test Branch code new one" );
+	  System.out.println( "Adding New line" );
     }
 }
